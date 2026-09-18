016cbcb10664e861a22bfc598d5f0469a79dfb10
KARANGULABASANTH <basanth9d@gmail.com>
2026-09-19 01:02:51 +0530

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,132 @@
+// 6
+// import java.util.*;
+// class day1{
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter a number");
+//         int n=sc.nextInt();
+//         for (int i=n;i>=1;i--)
+//         {
+//             for (int j=1;j<=i;j++)
+//             {
+//                 System.out.print(j+" ");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+
+//     }
+// }
+
+
+//11)
+
+// import java.util.*;
+// class day1{
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter a number");
+//         int n=sc.nextInt();
+//         for (int i=1;i<=n;i++)
+//         {
+//             for (int j=1;j<=i;j++)
+//             {
+//                 if ((i+j)%2==0)
+//                 {
+//                     System.out.print(1+" ");
+//                 }
+//                 else 
+//                 {
+//                     System.out.print(0+" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+
+//12)
+
+// import java.util.*;
+// class day1{
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter number of rows");
+//         int n=sc.nextInt();
+//         for (int i=1;i<=n;i++)
+//         {
+//             for (int j=1;j<=i;j++)
+//             {
+//                 System.out.print(j+" ");
+//             }
+//             for (int k=i;k<n;k++)
+//             {
+//                 System.out.print("    ");
+//             }
+//             for (int p=i;p>=1;p--)
+//             {
+//                 System.out.print(p+" ");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+
+//13)
+
+// import java.util.*;
+// class day1{
+//     public static void main(String args[])
+//     {
+//         int n=1;
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter the number");
+//         int z=sc.nextInt();
+//         for (int i=1;i<=z;i++)
+//         {
+//             for (int j=1;j<=i;j++)
+//             {
+//                 System.out.print(n+" ");
+//                 n+=1;
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+
+//21)
+
+import java.util.*;
+class day1{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=s.nextInt();
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n;j++)
+            {
+                if (i==1||j==1||i==n||j==n)
+                {
+                    System.out.print("*"+" ");
+                }
+                else 
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
---
+++ day1.py
@@ -0,0 +1,8 @@
+# print(ord('A')) 
+
+#14)
+# n=int(input("Enter a number"))
+# for i in range (0,n):
+#     for j in range (0,i+1):
+#         print(chr(ord('A')+j),end=" ")
+#     print()
