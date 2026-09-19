495a114a61cff93f6ac9c6d9b16e548acd85f0b6
KARANGULABASANTH <basanth9d@gmail.com>
2026-09-19 21:17:57 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,104 @@
+//14)
+
+// import java.util.*;
+// public class day2 {
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter a number");
+//         int n=sc.nextInt();
+//         int a=(int)('A');
+//         for (int i=0;i<n;i++)
+//         {
+//             for (int j=0;j<=i;j++)
+//             {
+//                 System.out.print((char)(a+j)+" ");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+//16)
+
+// import java.util.*;
+// class day2{
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter a number");
+//         int n=sc.nextInt();
+//         int p=(int)('A');
+//         for (int i=0;i<n;i++)
+//         {
+//             for (int j=0;j<=i;j++)
+//             {
+//                 System.out.print((char)(p+i)+" ");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+
+//10)
+
+// import java.util.*;
+// class day2{
+//     public static void main(String args[])
+//     {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter number of rows");
+//         int n=sc.nextInt();
+//         for (int i=1;i<=n/2+1;i++)
+//         {
+//             for (int j=1;j<=i;j++)
+//             {
+//                 System.out.print("*"+" ");
+//             }
+//             System.out.println();
+//         }
+//         for (int i=(n/2);i>=1;i--)
+//         {
+//             for (int j=1;j<=i;j++)
+//             {
+//                 System.out.print("*"+" ");
+//             }
+//             System.out.println();
+//         }
+//         sc.close();
+//     }
+// }
+
+
+//7)
+
+
+import java.util.*;
+class day2{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        sc.close();
+        for (int i=0;i<=n;i++)
+        {
+            for (int j=0;j<=n-i-1;j++)
+            {
+                System.out.print("   ");
+            }
+            for (int k=1;k<=(2*i)-1;k++)
+            {
+                System.out.print("*"+"  ");
+            }
+            for (int z=1;z<=n-i-1;z++)
+            {
+                System.out.print("   ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ day2.py
@@ -0,0 +1,7 @@
+# n=int(input("Enter a number"))
+# o=ord('A')
+# ans=o+n-1
+# for i in range (1,n+1):
+#     for j in range (1,i+1):
+#         print(chr(ans-(i-j)),end=" ")
+#     print()
